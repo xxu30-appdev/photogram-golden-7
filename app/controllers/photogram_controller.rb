1dcda1efcc304c43dfd4f5a8944f8e8ae4ec19b8
Sean Xu <xxu30@chicagobooth.edu>
2018-10-31 21:59:37 +0000

    final submission

--- app/controllers/photogram_controller.rb
+++ app/controllers/photogram_controller.rb
@@ -1,14 +1,73 @@
 class PhotogramController < ApplicationController
   def main_form
+    @photos = Photo.where("1=1")
+    #@photos = Photo.find(:all)
+    @img_ids = []
+    @img_captions = []
+    @img_urls = []
+    @img_photos = []
+    Photo.all.each do |photo|
+      @img_ids.push(photo.id)
+      @img_captions.push(photo.caption)
+      @img_urls.push(photo.source)
+      @img_photos.push(photo)
+    end
     render("layouts/index.html.erb")
   end
+  
   def new_photo
     render("layouts/new_photo.html.erb")
   end
+  
+  def show
+    the_id = params[:the_id]
+    p = Photo.find(the_id)
+    @img_cap = p.caption
+    @img_url = p.source
+    @img_mde = "?Unknown"
+    render("layouts/photo.html.erb")
+  end
+  
+  def edit_form
+      the_id = params[:an_id]
+      @img_id = the_id
+      p = Photo.find(the_id)
+      @img_cap = p.caption
+      @img_url = p.source
+      @img_mde = "?Unknown"
+      render("layouts/edit.html.erb")
+  end
+  
+  def edit_photo
+      @num_photos = Photo.count
+      @img_id = params[:the_id].to_i
+      @img_url = params[:the_source]
+      @img_cap = params[:the_caption]
+      p = Photo.find(@img_id)
+      p.caption = @img_cap
+      p.source = @img_url
+      p.save
+      redirect_to("/photos/" + @img_id.to_s)
+  end
+  
   def create_photo
-    @num_photos = 0
+    @num_photos = Photo.count
     @img_url = params[:the_source]
     @img_cap = params[:the_caption]
-    render("layouts/create_photo.html.erb")
+    p = Photo.new
+    p.source = @img_url
+    p.caption= @img_cap
+    p.save
+    redirect_to("/photos")
+    #render("layouts/create_photo.html.erb")
+  end
+  
+  def destroy_row
+    the_id = params[:toast_id].to_i
+    p = Photo.find(the_id)
+    p.destroy
+    @num_photos = Photo.count
+    redirect_to("/photos")
+    #render("layouts/delete_photo.html.erb")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
+  
   get("/", { :controller => "photogram", :action => "main_form" })
+  get("/photos", { :controller => "photogram", :action => "main_form" })
   get("/photos/new", { :controller => "photogram", :action => "new_photo" })
   get("/create_photo", { :controller => "photogram", :action => "create_photo" })
-  get("/photos/:the_id", { :controller => "pictures", :action => "show" })
-  get("/photos/:an_id/edit", { :controller => "pictures", :action => "edit_form" })
-  get("/update_photo/:some_id", { :controller => "pictures", :action => "update_row" })
-  get("/delete_photo/:toast_id", { :controller => "pictures", :action => "destroy_row" })
+  get("/photos/:the_id", { :controller => "photogram", :action => "show" })
+  get("/edit_photo", { :controller => "photogram", :action => "edit_photo" })
+  get("/photos/:an_id/edit", { :controller => "photogram", :action => "edit_form" })
+  get("/update_photo/:some_id", { :controller => "photogram", :action => "update_row" })
+  get("/delete_photo/:toast_id", { :controller => "photogram", :action => "destroy_row" })
 end
